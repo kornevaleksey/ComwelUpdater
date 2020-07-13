3f48ff9e79573e7322ddb1e0aec13ed43bde8f34
korall <korall@olvs.miee.ru>
2020-07-13 15:14:05 +0300

    Initial

---
+++ Config/IConfigUpdater.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace ConfigUpdater
+{
+    public interface IConfigUpdater
+    {
+        Dictionary<string, string> Config { get; }
+        string FileName { get; set; }
+        
+        Task<bool> ReadAsync();
+        Task<bool> WriteAsync();
+        void SetDefault();
+    }
+}
